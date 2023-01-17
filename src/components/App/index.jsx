9015ffbf7d16a3241e77b71a873ea2d08a7be927
Marc Moncusi Rubio <moncusimarc@gmail.com>
2023-01-17 09:56:53 +0100

    reducer

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,11 +1,26 @@
+import axios from 'axios'
 import { SearchBox, Window } from './styles.js'
+import { legacy_createStore as createStore } from 'redux'
+import { dictionaryReducer, insertWord } from '../Reducers/dictionaryReducer.js'
+import { Provider } from 'react-redux'
+import TestComponent from '../TestComponent/TestComponent.js'
+
+const store = createStore(dictionaryReducer)
+
+axios.get('https://api.dictionaryapi.dev/api/v2/entries/en/hello').then(response => {
+  console.log(response.data[0].word)
+  store.dispatch(insertWord(response.data[0]))
+})
 
 function App () {
   return (
-    <Window className='window'>
-      <SearchBox className='searchBox'>__
-      </SearchBox>
-    </Window>
+    <Provider store={store}>
+      <Window className='window'>
+        <SearchBox className='searchBox'>__
+        </SearchBox>
+        <TestComponent />
+      </Window>
+    </Provider>
   )
 }
 
---
+++ src/components/TestComponent/TestComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+const TestComponent = () => {
+  const Words = useSelector((state) => state)
+  console.log(Words)
+  return (
+    <div>
+      {Words.map((word) => (<p key={word.word}> {word.word}</p>))}
+    </div>
+  )
+}
+
+export default TestComponent
